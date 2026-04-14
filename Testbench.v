bfad6d88145c75d703c05790ae0b93caecd36f01
Ritav <ritav08092004@gmail.com>
2026-04-14 18:08:34 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.04.2026 12:19:03
-// Design Name: 
-// Module Name: tb_pEncoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_pEncoder();
 reg a_i;
